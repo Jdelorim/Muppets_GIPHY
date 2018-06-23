d2dfa083c873d669f3fc19764b3d7e3bbc7bd663
Jdelorim <joshuadelorimier@gmail.com>
2018-06-23 19:30:50 -0400

    finished working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-
-
-
-
 var mTopics = ["Kermit","Miss Piggy","Gonzo","Fozzie Bear","Animal","Flyod Pepper","Scooter","Rowlf the Dog","Beaker","Bunsen Honeydew","Sam Eagle","The Swedish Chef","Dr. Teeth","Stalter and Waldorf","Camilla the Chicken","Bobo the Bear","Bean Bunny","Rizzo the Rat"];
-
-
-
 
 for(var i = 0; i < mTopics.length; i++) {
     var topButton = $("<button>");
@@ -14,30 +7,22 @@ for(var i = 0; i < mTopics.length; i++) {
     topButton.attr("value",mTopics[i]);
     topButton.html( mTopics[i]);
     $("#putTopics").append(topButton);
-    
 }
 
-
-$("#sendMuppet").on('click',function(){
+$("#sendMuppet").on('click',function() {
     var nButton = $("<button>");
     var getVfrombox = $('#getMuppet').val();
     nButton.addClass("jbutton");
     nButton.attr("value",getVfrombox);
-    mButton.html(getVfrombox);
-    $('#putTopics').append(mButton);
-   
-
+    nButton.html(getVfrombox);
+    $('#putTopics').append(nButton);
 });
 
+$(document).on("click",".jbutton",function(){
 
-$(".jbutton").on('click',function(){
-
-
-
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).val()  + "AND muppets" +
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).val()  + "AND Muppets" +
 "&api_key=CcXigouHSYxnSCDQSR1zhiEjhmkgATsb&limit=10"
 
-//CcXigouHSYxnSCDQSR1zhiEjhmkgATsb
 $.ajax({
     url: queryURL,
     method: "GET"
@@ -50,22 +35,30 @@ $.ajax({
         var jDiv = $("<div class='jgif'>");
         var p = $('<p>').text("Rating: " + rating);
         var muppetImg = $("<img>");
-        muppetImg.attr("src",results[i].images.fixed_height.url);
+        
+        muppetImg.attr("src",results[i].images.fixed_height_still.url);
+        muppetImg.attr("still",results[i].images.fixed_height_still.url);
+        muppetImg.attr("animate",results[i].images.fixed_height.url);
+        muppetImg.attr("data-state","still");
         jDiv.append(p);
         jDiv.append(muppetImg);
         $("#putGIFS").prepend(jDiv);
-
-    }
-
-
-})
-   
+      
+        }
+    })
 });
 
+$(document).on("click",".jgif",function(){
 
-
-
-
-
-
+  var getImg = $(this).find('img');  
+  var state = $(this).find('img').attr("data-state");
+ 
+  if(state === "still") {
+    getImg.attr("src",getImg.attr("animate"));
+    getImg.attr("data-state","animate");
+  } else {
+      getImg.attr("src",getImg.attr("still"));
+      getImg.attr("data-state","still");
+  }
+});
 });
